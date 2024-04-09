31f23ee5bc6448221f764c086bfbf674bec9dbe6
menteLocuaz <emersonduarte.45@gmail.com>
2024-04-09 16:13:11 -0300

    add estatus a an class

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    # añadimo nuestro propio satus
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
     title = models.CharField(max_length=250)
     slug = models.CharField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
 
     # metemos los meta datos, lo organisamos en decenso y de abajo hacia arriba
     # indesamos para mejora en ceo y la busqueda
