3bb39846cb57bf725e1025ea1d9016af460db06f
menteLocuaz <emersonduarte.45@gmail.com>
2024-04-09 16:46:07 -0300

    try ,execp , atrapando errores

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
-# Create your views here.
+
+def pos_list(request):
+    posts = Post.publish.all()
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request):
+    try:
+        post = Post.publish.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post Found")
+
+    return render(request, "blog/post/detail.html", {"post": post})
