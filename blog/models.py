ca502c528383d60bed521a1650ff5dd1c200bf37
menteLocuaz <emersonduarte.45@gmail.com>
2024-04-12 15:45:45 -0300

    creando url amigables para el usuario y seo de buscador

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -38,7 +38,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
     # la url canonical que se comunica con url absoluta
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.id])
-    
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ app_name = "blog"
 
 urlpatterns = [
     path("", pos_list, name="post_list"),
-    path("<int:id>/", post_detail, name="post_detail"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"
+    ),
 ]
