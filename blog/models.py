aaae9b8f5535ab95acc44b89410034a7b649caa8
menteLocuaz <emersonduarte.45@gmail.com>
2024-04-12 15:01:56 -0300

    css y   link basicos

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # esta clase sirve para el mager y solo publica los Punlisead
